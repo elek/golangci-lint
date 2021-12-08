d3caf7d3431d732ff06186f548a89877a7a03565
Márton Elek <elek@apache.org>
2021-12-08 17:32:02 +0100

    improve fork to make it 1.14.0 compatible

--- fork.sh
+++ fork.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-srcdirs=( internal pkg cmd )
+set -ex
+
+srcdirs=(test scripts internal pkg cmd )
 for dir in "${srcdirs[@]}"; do
+   find $dir -name "*.go" | xargs -n1 sed -i 's/github.com\/golangci\/golangci-lint\/test\//github.com\/elek\/golangci-lint\/test\//g'
    find $dir -name "*.go" | xargs -n1 sed -i 's/github.com\/golangci\/golangci-lint\/pkg\//github.com\/elek\/golangci-lint\/pkg\//g'
    find $dir -name "*.go" | xargs -n1 sed -i 's/github.com\/golangci\/golangci-lint\/internal\//github.com\/elek\/golangci-lint\/internal\//g'
 done
